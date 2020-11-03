7d519bb59cc1a94e2c172ab0a01b9897c977a104
harriet-the-spy <dpcairns@gmail.com>
2020-11-02 16:42:12 -0800

    add banjos . . . again!

---
+++ data/banjos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    brand: 'gold tone',
+    noise_level: 3,
+  },
+  {
+    brand: 'fender',
+    noise_level: 4,
+  },
+  {
+    brand: 'deering',
+    noise_level: 10,
+  }
+];
