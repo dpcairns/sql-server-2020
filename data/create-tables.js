b6ded588f93c43a1fcbbd972411f1411ff3bc430
harriet-the-spy <dpcairns@gmail.com>
2020-11-02 16:39:54 -0800

    revert

--- data/create-tables.js
+++ data/create-tables.js
@@ -11,18 +11,16 @@ async function run() {
     await client.connect();
 
     // run a query to create tables
-
-    // data model, SQL Schema, SQL Table definition
     await client.query(`
                 CREATE TABLE users (
                     id SERIAL PRIMARY KEY,
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE banjos (
+                CREATE TABLE animals (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    brand VARCHAR(512) NOT NULL,
-                    noise_level INTEGER NOT NULL,
+                    name VARCHAR(512) NOT NULL,
+                    cool_factor INTEGER NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const banjos = require('./banjos.js');
+const animals = require('./animals.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      banjos.map(banjo => {
+      animals.map(animal => {
         return client.query(`
-                    INSERT INTO banjos (brand, noise_level, owner_id)
+                    INSERT INTO animals (name, cool_factor, owner_id)
                     VALUES ($1, $2, $3);
                 `,
-        [banjo.brand, banjo.noise_level, user.id]);
+        [animal.name, animal.cool_factor, user.id]);
       })
     );
     
